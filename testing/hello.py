d38bc79aff185335c488d9537f4e0a896cb8d989
sparlor <sparlor91@gmail.com>
2020-10-04 18:38:24 -0400

    initial

--- testing/hello.py
+++ testing/hello.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
-driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+driver = webdriver.Chrome('./testing/chromedriver', options=chrome_options)
 driver.get("http://192.168.99.121:30880")
 
 hello_text = driver.find_element(By.XPATH, "//*[text()='Hello, World!']")
